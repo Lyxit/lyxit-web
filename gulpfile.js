371b672f9ee15d2791aa8b1d5f350538283555f4
= <anton@lyxit.se>
2015-07-22 12:45:37 +0200

    Cleaned up project

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,22 @@
-var glob      = require('glob'),
-    gulp      = require('gulp'),
-    less      = require('gulp-less'),
-    jade      = require('gulp-jade'),
-    concat    = require('gulp-concat');
+var glob              = require('glob'),
+    gulp              = require('gulp'),
+    del               = require('del'),
+    less              = require('gulp-less'),
+    autoprefixplugin  = require('less-plugin-autoprefix'),
+    autoprefix        = new autoprefixplugin({ browsers: ["last 2 versions"] }),
+    jade              = require('gulp-jade'),
+    concat            = require('gulp-concat');
 
 /* DATA */
 
 var paths = {
-
+  index: './src/index.jade',
+  project_data: './src/data/project-list.data',
+  templates: './src/templates/',
+  src_assets: './src/assets/',
+  build: './build/',
+  build_assets: './build/assets/',
+  build_css: './build/assets/css/'
 };
 
 var msg = {
@@ -16,44 +25,32 @@ var msg = {
 
 /* TASKS */
 
-var CONFIG;
+gulp.task('default', ['clean:build', 'build', 'index', 'less']);
 
-gulp.task('default', ['index', 'projects']);
-
-gulp.task('projects', function(cb) {
-  console.log ('RUNNING CONFIG!');
-  glob("./src/data/*.project", function (er, files) {
-    for (var i = 0; i < files.length; i++) {
-      console.log ('RUNNING PROJECT: ' + files[i]);
-      CONFIG = require(files[i]);
-      CONFIG.parts = partsToPaths(CONFIG.parts);
-      console.log ('CONFIG PARTS: ' + CONFIG.parts);
-
-      gulp.src(CONFIG.parts)
-      .pipe(concat('index.jade'))
-      .pipe(jade({
-        data: CONFIG,
-        pretty: true
-      }))
-      .pipe(gulp.dest('./build/projects/' + CONFIG.name.toLowerCase()));
-    }
-  });
+gulp.task('clean:build', function(cb) {
+  del([
+    'build/**/*'
+  ], cb);
 });
 
-gulp.task('index', function() {
-  gulp.src('./src/index.jade')
+gulp.task('less', ['clean:build'], function() {
+  gulp.src('./src/less/style.less')
+  .pipe(less({
+    plugins: [autoprefix]
+  }))
+  .pipe(gulp.dest(paths.build_css));
+});
+
+gulp.task('index', ['clean:build'], function() {
+  gulp.src(paths.index)
   .pipe(jade({
-    data: require('./src/data/project-list.data'),
+    data: require(paths.project_data),
     pretty: true
   }))
-  .pipe(gulp.dest('./build/'))
+  .pipe(gulp.dest(paths.build))
 });
 
-/* HELPERS */
-var partsToPaths = function(parts) {
-  console.log ('PARTS TO PATHS!!');
-  for (var i = 0; i < parts.length; i++) {
-    parts[i] = './src/templates/' + parts[i] + '.jade';
-  }
-  return parts;
-}
+gulp.task('build', ['clean:build'], function() {
+  gulp.src(paths.src_assets + '**/*')
+  .pipe(gulp.dest(paths.build_assets))
+});
