00946b8846ee97d6f7598e0dfc2368242a02f911
= <anton@lyxit.se>
2015-10-08 10:20:56 +0200

    Improve build script
    Add color to build process
    Add support for website tiles

--- gulpfile.js
+++ gulpfile.js
@@ -5,52 +5,84 @@ var glob              = require('glob'),
     autoprefixplugin  = require('less-plugin-autoprefix'),
     autoprefix        = new autoprefixplugin({ browsers: ["last 2 versions"] }),
     jade              = require('gulp-jade'),
-    concat            = require('gulp-concat');
+    concat            = require('gulp-concat'),
+    inject            = require('gulp-inject'),
+    insert            = require('gulp-insert');
 
 /* DATA */
 
 var paths = {
   index: './src/index.jade',
   project_data: './src/data/project-list.data',
-  templates: './src/templates/',
-  src_assets: './src/assets/',
+  src: './src/assets/**/*',
   build: './build/',
-  build_assets: './build/assets/',
-  build_css: './build/assets/css/'
-};
-
-var msg = {
-
+  less: './src/less/',
+  temp: './build/temp/'
 };
 
 /* TASKS */
-
-gulp.task('default', ['clean:build', 'build', 'index', 'less']);
-
-gulp.task('clean:build', function(cb) {
+gulp.task('build', ['jade', 'css'], function() {
   del([
-    'build/**/*'
-  ], cb);
+    'build/temp/'
+  ]);
+
+  return gulp
+  .src(paths.src)
+  .pipe(gulp.dest(paths.build + 'assets/'))
 });
 
-gulp.task('less', ['clean:build'], function() {
-  gulp.src('./src/less/style.less')
-  .pipe(less({
-    plugins: [autoprefix]
-  }))
-  .pipe(gulp.dest(paths.build_css));
+gulp.task('clean', function() {
+  del([
+      'build/**/*'
+  ]);
 });
 
-gulp.task('index', ['clean:build'], function() {
-  gulp.src(paths.index)
-  .pipe(jade({
-    data: require(paths.project_data),
-    pretty: true
-  }))
-  .pipe(gulp.dest(paths.build))
+gulp.task('insert', ['insert:colors', 'insert:styles']);
+
+gulp.task('insert:colors', function() {
+  var lesscolors = "";
+
+  var data = require(paths.project_data);
+  for(var prop in data.projects) {
+    lesscolors = lesscolors + '@' + prop + '-color: ' + data.projects[prop].color + ';';
+  }
+
+  return gulp
+    .src(paths.less + 'vars.less')
+    .pipe(insert.append(lesscolors))
+    .pipe(gulp.dest(paths.temp + 'less'));
 });
 
-gulp.task('build', ['clean:build'], function() {
-  gulp.src(paths.src_assets + '**/*')
-  .pipe(gulp.dest(paths.build_assets))
+gulp.task('insert:styles', function() {
+  var lesstyles = "";
+
+  var data = require(paths.project_data);
+  for(var prop in data.projects) {
+    var type = data.projects[prop].web ? "web" : "icon";
+    lesstyles = lesstyles + '.' + prop + '{ background: @' + prop + '-color; a .' + type + 'image { background-image: url("../img/' + prop + '.png")}}';
+  }
+
+  return gulp
+    .src(paths.less + 'style.less')
+    .pipe(insert.append(lesstyles))
+    .pipe(gulp.dest(paths.temp + 'less'));
+});
+
+gulp.task('css', ['insert'], function() {
+    return gulp
+    .src(paths.temp + 'less/style.less')
+    .pipe(less({
+        plugins: [autoprefix]
+    }))
+    .pipe(gulp.dest(paths.build + 'assets/css/'));
+});
+
+gulp.task('jade', function() {
+    return gulp
+    .src(paths.index)
+    .pipe(jade({
+        data: require(paths.project_data),
+        pretty: true
+    }))
+    .pipe(gulp.dest(paths.build))
 });
